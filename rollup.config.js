b63bf3908157dcc559d69db1fa3e31a2d2564726
JangHyeok Kim <d0or.hyeok@gmail.com>
2024-01-10 16:42:44 +0900

    chore: build settings

--- rollup.config.js
+++ rollup.config.js
@@ -4,14 +4,21 @@ const terser = require('@rollup/plugin-terser');
 const { dts } = require('rollup-plugin-dts');
 const { default: esbuild } = require('rollup-plugin-esbuild');
 const createBabelConfig = require('./babel.config.js');
+const pkg = require('./package.json');
 
 const extensions = ['.js', '.ts'];
+
+const banner =
+  `// ${pkg.name.toUpperCase()} v${pkg.version}\n` +
+  `// ${pkg.repository.url}\n` +
+  `// (c) 2024 - ${new Date().getFullYear()} ${pkg.author}\n` +
+  '// Doumi may be freely distributed under the MIT license.\n';
 
 function getBabelOptions(targets) {
   return {
     ...createBabelConfig({ env: (env) => env === 'build' }, targets),
     extensions,
-    comments: false,
+    comments: true,
     babelHelpers: 'bundled',
   };
 }
@@ -37,7 +44,7 @@ function createDeclarationConfig(input, output) {
 function createESMConfig(input, output) {
   return {
     input,
-    output: { file: output, format: 'esm' },
+    output: { file: output, format: 'esm', banner },
     plugins: [getEsbuild('node12')],
   };
 }
@@ -49,13 +56,14 @@ function createCommonJSConfig(input, output) {
       file: `${output}.js`,
       format: 'cjs',
       esModule: false,
+      banner,
     },
     plugins: [babelPlugin(getBabelOptions({ ie: 11 }))],
   };
 }
 
 function createUMDConfig(input, output, env) {
-  let name = 'doumi';
+  let name = pkg.name;
   const fileName = output.slice('dist/umd/'.length);
   const capitalize = (s) => s.slice(0, 1).toUpperCase() + s.slice(1);
   if (fileName !== 'index') {
@@ -67,6 +75,7 @@ function createUMDConfig(input, output, env) {
       file: `${output}.${env}.js`,
       format: 'umd',
       name,
+      banner,
     },
     plugins: [
       babelPlugin(getBabelOptions({ ie: 11 })),
@@ -84,6 +93,7 @@ module.exports = function (args) {
   }
   return [
     createDeclarationConfig(`src/${target}.js`, 'dist'),
+    createDeclarationConfig(`src/${target}.js`, 'dist/esm'),
     createCommonJSConfig(`src/${target}.js`, `dist/${target}`),
     createESMConfig(`src/${target}.js`, `dist/esm/${target}.js`),
     createUMDConfig(`src/${target}.js`, `dist/umd/${target}`, 'development'),
