5c598d5a90d50d1eee9b35a3a6e10aea1b4908ad
JangHyeok Kim <d0or.hyeok@gmail.com>
2024-01-31 10:04:34 +0900

    fix: intersection method

--- packages/doumi/src/array/intersection.ts
+++ packages/doumi/src/array/intersection.ts
@@ -1,38 +1,59 @@
-import { filter, some } from '../collection';
 import { isFunction } from '../lang';
 
 /**
  * Creates an array of values that are included in all given arrays. The order and references of result values are determined by the first array.
  * @since 0.1.0
  * @template T
- * @param {T[]} array The array to inspect.
- * @param {T[][] | [...T[][], (value: T, index: number) => any]} other The values to exclude.
+ * @param {T[][] | [...T[][], (value: T, index: number) => any]} arrays The arrays to inspect.
  * @param [transformer] The transformer invoked per element for comparison.
  * @returns {T[]}
  * @example
  *
  * const arr = [1, 2, 3, 4, 5]
  * const other = [3, 5, 6]
- * intersection(arr, other) // [1, 2, 4]
+ * intersection(arr, other) // [3, 5]
+ * intersection(arr, other, [5], (value) => value > 3) // [4, 5]
  */
 function intersection<T>(
-  array: T[],
-  ...other: T[][] | [...T[][], (value: T, index: number) => any]
+  ...arrays: T[][] | [...T[][], (value: T, index: number) => any]
 ): T[] {
   let transformer: (...arg: any[]) => any = (v: any) => v;
-  const lastItem = other.at(-1);
-  let o = other as T[][];
+  const lastItem = arrays.at(-1);
+  let arrs = arrays as T[][];
   if (isFunction(lastItem)) {
     transformer = lastItem;
-    o = other.slice(0, -1) as T[][];
+    arrs = arrays.slice(0, -1) as T[][];
   }
-
-  return filter(array, (value, index) => {
-    const converted = transformer(value, index);
-    return o.every((arr) =>
-      some(arr, (item, i) => transformer(item, i) == converted)
-    );
-  });
+  const map: Map<
+    any,
+    { values: Set<any>; count: number; [idx: number]: boolean }
+  > = new Map();
+  const arrsNum = arrs.length;
+  arrs.forEach((arr, index) =>
+    arr.forEach((item, i) => {
+      const converted = transformer(item, i);
+      const data = map.get(converted);
+      if (data) {
+        data.values.add(item);
+        if (!data[index]) {
+          data.count += 1;
+          data[index] = true;
+        }
+      } else if (index === 0) {
+        map.set(converted, {
+          values: new Set([item]),
+          count: 1,
+          [index]: true,
+        });
+      }
+    })
+  );
+  const results: T[] = [];
+  Array.from(map.values()).forEach(
+    ({ values, count }) =>
+      count === arrsNum && results.push(...Array.from(values.values()))
+  );
+  return results;
 }
 
 export default intersection;
--- packages/doumi/tests/array.test.js
+++ packages/doumi/tests/array.test.js
@@ -19,22 +19,16 @@ describe('ARRAY TEST', () => {
   });
 
   it('intersection', () => {
-    let resutls = intersection([1, 2, 3, 4, 5], [2, 3, 4], [2, 3]);
-    expect(JSON.stringify(resutls)).toBe(JSON.stringify([2, 3]));
+    let resutls = intersection([1, 2, 3, 4, 5], [3, 5, 6]);
+    expect(JSON.stringify(resutls)).toBe(JSON.stringify([3, 5]));
 
     resutls = intersection(
-      [
-        { name: 'a', score: 50 },
-        { name: 'b', score: 30 },
-        { name: 'c', score: 60 },
-        { name: 'd', score: 30 },
-      ],
-      [
-        { name: 'e', score: 65 },
-        { name: 'f', score: 100 },
-      ],
-      (v) => v.score > 50
+      [1, 2, 3, 4, 5],
+      [3, 5, 6],
+      [5],
+      (value) => value > 3
     );
+    expect(JSON.stringify(resutls)).toBe(JSON.stringify([4, 5, 6]));
   });
 
   it('range', () => {
